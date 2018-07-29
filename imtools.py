b2d225bfcf8dbd9d00312331a317040c1ae6cb8a
Roman Ilechko <rooooman69@gmail.com>
2018-07-29 22:29:24 +0300

    Add files via upload
    added split function

--- imtools.py
+++ imtools.py
@@ -1,4 +1,14 @@
-iport os
+import os
 
 def get_imlist(path):
     return [os.path.join(path,f) for f in os.listdir(path) if f.endswith('.jpg')]
+
+def split(path, path_test, path_train,for_train=0.8):
+    imgs = get_imlist(path)
+    count = int(len(imgs) * for_train)
+    for index, img in enumerate(imgs):
+        if index < count:
+            os.rename(img, path_train)
+        else:
+            os.rename(img, path_test)
+    
