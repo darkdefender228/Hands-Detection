3809b70a3349b6579b2b4e0995c1a70d67630eb1
Roman Ilechko <rooooman69@gmail.com>
2018-07-31 20:08:03 +0300

    Add files via upload

--- imtools.py
+++ imtools.py
@@ -1,4 +1,5 @@
 import os
+from sklearn.decomposition import PCA
 
 def get_imlist(path):
     return [os.path.join(path,f) for f in os.listdir(path) if f.endswith('.jpg')]
@@ -11,4 +12,13 @@ def split(path, path_test, path_train,for_train=0.8):
             os.rename(img, path_train)
         else:
             os.rename(img, path_test)
-    
+            
+def reduce_dim(n_comp, img):
+    """
+    reduce dimension of image using PCA
+    """
+    pca = PCA(n_components = n_comp)
+    pca.fit(img)
+    gray_pca = pca.fit_transform(img)
+    img_restored = pca.inverse_transform(gray_pca)
+    return img_restored
